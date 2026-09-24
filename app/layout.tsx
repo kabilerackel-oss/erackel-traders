b91bbbd506305c840ec0ac95fa0d1d38cc0601f6
kabilerackel-oss <kabilerackel@gmail.com>
2026-09-24 12:40:24 +0530

    Import TawkChat component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TawkChat from "@/components/TawkChat";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
